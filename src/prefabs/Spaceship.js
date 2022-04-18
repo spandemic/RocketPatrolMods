d6422f6b83da5941d123d5fad87fa12e4369a238
spandemic <anloeng@ucsc.edu>
2022-04-18 13:21:04 -0700

    test commit

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,20 @@
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+      super(scene, x, y, texture, frame);
+  
+      // add object to existing scene
+      scene.add.existing(this);
+      this.points = pointValue;
+      this.moveSpeed = 3;
+    }
+    update() {
+        this.x -= this.moveSpeed;
+        if(this.x <= 0 - this.width) {
+            this.reset();
+        }
+    }
+    
+    reset() {
+        this.x = game.config.width;
+    }
+}
